456c3ba40fb0a33b5ff291835f4d26c891da2db2
MatteD04 <matteo.dantonio04@gmail.com>
2024-04-10 17:27:49 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+  import AppTitle from './components/AppTitle.vue';
+
+  export default {
+    components: {
+      AppTitle
+    }
+  }
+</script>
+
+<template>
+  <main>
+    <AppTitle></AppTitle>
+  </main>
+</template>
